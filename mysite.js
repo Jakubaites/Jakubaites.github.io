f438cc8b79962f59606d1c958b26e37e973e01d3
Jakubaites <109096238+Jakubaites@users.noreply.github.com>
2023-02-20 19:28:05 +0300

    Add files via upload

---
+++ mysite.js
@@ -0,0 +1,20 @@
+﻿window.onload = init;
+
+function init() {
+       navbar();
+};
+
+function navbar() {
+      let menu = document.querySelector('.menu_body');
+       let menuButton = document.querySelector('.menu__icon');
+       let body = document.body;
+
+       if (menu && menuButton) {
+       menuButton.addEventListener('click', e => {
+            menu.classList.toggle('active');
+            menuButton.classList.toggle('active');
+            body.classList.toggle('lock');
+       })
+       };         
+      
+};
